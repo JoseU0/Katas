a6ef6edcdb538058ade4cafd33b79b433bc77489
JoseU0 <99227481+JoseU0@users.noreply.github.com>
2022-02-10 15:24:12 -0800

    Add files via upload

---
+++ Ejercicio Kata1.py
@@ -0,0 +1,6 @@
+from datetime import date
+print("Today's date is: " + str(date.today()))
+
+parsec = 11
+lightyears = parsec*3.26156
+print(str(parsec) + " parsec are " + str(lightyears) + " lightyears")
