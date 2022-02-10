780870dfa4fd978b5606820ad71164fb098e6638
JoseU0 <99227481+JoseU0@users.noreply.github.com>
2022-02-10 10:36:33 -0800

    Delete Ejercicio Kata1.py
    Incorrect file

--- Ejercicio Kata1.py
+++
@@ -1,8 +0,0 @@
-""" print ("Bienvenido al programa de bienvenida")
-name = input ("Introduzca su nombre ")
-print ("Saludos : " + name)
- """
-print ("Calculadora")
-fn = input("Primer num ")
-sn = input("Segundo num ")
-print(fn + sn)
